a80c72440acc730dcac9efd02b186aa7647d4e54
Saulo <saulo.nuez102@alu.ulpgc.es>
2023-06-07 22:35:39 +0100

    Implementando SplashScreen

--- app/src/main/java/com/example/sauloandroidpem/MainActivity.kt
+++ app/src/main/java/com/example/sauloandroidpem/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -33,6 +34,9 @@ class MainActivity : ComponentActivity() {
     lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             SauloAndroidPEMTheme {
                 // A surface container using the 'background' color from the theme
